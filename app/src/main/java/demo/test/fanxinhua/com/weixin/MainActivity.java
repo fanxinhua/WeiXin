571875bc194265efa530f22aef169a955b5d55cd
fxh123456 <1464443540@qq.com>
2018-01-09 14:00:03 +0800

    gbhn

--- app/src/main/java/demo/test/fanxinhua/com/weixin/MainActivity.java
+++ app/src/main/java/demo/test/fanxinhua/com/weixin/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+      int a;
 
     }
 
