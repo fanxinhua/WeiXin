2e9d120d5c24fad478f23ae1faf272859787bd1d
fxh123456 <1464443540@qq.com>
2018-01-09 13:57:18 +0800

    gbhn

---
+++ app/src/main/java/demo/test/fanxinhua/com/weixin/DataSynEvent.java
@@ -0,0 +1,21 @@
+package demo.test.fanxinhua.com.weixin;
+
+/**
+ * Created by dell on 2018/1/9.
+ */
+
+public class DataSynEvent {
+    String name;
+
+    public DataSynEvent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
